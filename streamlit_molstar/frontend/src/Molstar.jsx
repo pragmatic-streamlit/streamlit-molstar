2f9e9218bbf5ed75b3799bd01efdb8226c028f52
luowf <louwf@dp.tech>
2023-03-23 03:09:34 +0800

    add more

--- streamlit_molstar/frontend/src/Molstar.jsx
+++ streamlit_molstar/frontend/src/Molstar.jsx
@@ -1,17 +1,31 @@
 import React, { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import { DefaultPluginUISpec } from "molstar/lib/mol-plugin-ui/spec";
-
+import { QualityAssessmentPLDDTPreset, QualityAssessmentQmeanPreset } from 'molstar/lib/extensions/model-archive/quality-assessment/behavior';
+import { QualityAssessment } from 'molstar/lib/extensions/model-archive/quality-assessment/prop';
 import { TrajectoryFromModelAndCoordinates } from 'molstar/lib/mol-plugin-state/transforms/model';
 import { StateTransforms } from 'molstar/lib/mol-plugin-state/transforms';
 import { PluginConfig } from "molstar/lib/mol-plugin/config";
 import "molstar/build/viewer/molstar.css";
 import { ParamDefinition } from "molstar/lib/mol-util/param-definition";
 import { CameraHelperParams } from "molstar/lib/mol-canvas3d/helper/camera-helper";
-import { OpenFiles, DownloadFile } from 'molstar/lib/mol-plugin-state/actions/file';
+import { OpenFiles } from 'molstar/lib/mol-plugin-state/actions/file';
 import { Asset } from 'molstar/lib/mol-util/assets';
+import { PresetStructureRepresentations, StructureRepresentationPresetProvider } from 'molstar/lib/mol-plugin-state/builder/structure/representation-preset';
+import { StateObjectRef } from 'molstar/lib/mol-state';
+import { presetStaticComponent } from 'molstar/lib/mol-plugin-state/builder/structure/representation-preset';
+
+import { Material } from 'molstar/lib/mol-util/material';
+import { InteractionsRepresentationProvider } from 'molstar/lib/mol-model-props/computed/representations/interactions';
+import { InteractionTypeColorThemeProvider } from 'molstar/lib/mol-model-props/computed/themes/interaction-type';
+import { Color } from 'molstar/lib/mol-util/color';
+
 
 import { createPluginUI } from './create-plugin-ui';
+const CustomMaterial = Material({ roughness: 0.2, metalness: 0 });
+const PresetParams = {
+  ...StructureRepresentationPresetProvider.CommonParams,
+};
 
 
 export const addTrajectory = async (plugin, params) => {
@@ -77,6 +91,69 @@ export const addTrajectory = async (plugin, params) => {
 };
 
 
+
+
+const InteractionsPreset = StructureRepresentationPresetProvider({
+  id: 'preset-interactions',
+  display: { name: 'Interactions' },
+  params: () => PresetParams,
+  async apply(ref, params, plugin) {
+      const structureCell = StateObjectRef.resolveAndCheck(plugin.state.data, ref);
+      const structure = structureCell?.obj?.data;
+      if (!structureCell || !structure) return {};
+
+      const components = {
+          ligand: await presetStaticComponent(plugin, structureCell, 'all'),
+          interactions: await presetStaticComponent(plugin, structureCell, 'all'),
+      };
+
+      const { update, builder, typeParams } = StructureRepresentationPresetProvider.reprBuilder(plugin, params);
+      const representations = {
+          ligand: builder.buildRepresentation(update, components.ligand, { type: 'ball-and-stick', typeParams: { ...typeParams, material: CustomMaterial, sizeFactor: 0.3 }, color: 'element-symbol', colorParams: { carbonColor: { name: 'element-symbol', params: {} } } }, { tag: 'all' }),
+          ballAndStick: builder.buildRepresentation(update, components.surroundings, { type: 'ball-and-stick', typeParams: { ...typeParams, material: CustomMaterial, sizeFactor: 0.1, sizeAspectRatio: 1 }, color: 'element-symbol', colorParams: { carbonColor: { name: 'element-symbol', params: {} } } }, { tag: 'ball-and-stick' }),
+          interactions: builder.buildRepresentation(update, components.interactions, { type: InteractionsRepresentationProvider, typeParams: { ...typeParams, material: CustomMaterial, includeParent: true, parentDisplay: 'between' }, color: InteractionTypeColorThemeProvider }, { tag: 'interactions' }),
+          label: builder.buildRepresentation(update, components.surroundings, { type: 'label', typeParams: { ...typeParams, material: CustomMaterial, background: false, borderWidth: 0.1 }, color: 'uniform', colorParams: { value: Color(0x000000) } }, { tag: 'label' }),
+      };
+
+      await update.commit({ revertOnError: true });
+      plugin.managers.interactivity.setProps({ granularity: 'element' });
+
+      return { components, representations };
+  }
+});
+
+const ViewerAutoPreset = StructureRepresentationPresetProvider({
+  id: 'preset-structure-representation-viewer-auto',
+  display: {
+    name: 'Automatic (w/ Annotation)', group: 'Annotation',
+    description: 'Show standard automatic representation but colored by quality assessment (if available in the model).'
+  },
+  isApplicable(a) {
+    return (
+      !!a.data.models.some(m => QualityAssessment.isApplicable(m, 'pLDDT')) ||
+      !!a.data.models.some(m => QualityAssessment.isApplicable(m, 'qmean'))
+    );
+  },
+  params: () => StructureRepresentationPresetProvider.CommonParams,
+  async apply(ref, params, plugin) {
+    const structureCell = StateObjectRef.resolveAndCheck(plugin.state.data, ref);
+    const structure = structureCell && structureCell.obj && structureCell.obj.data;
+    if (!structureCell || !structure) return {};
+    
+    if (structure.model.sourceData.kind === 'gro'){
+      return await InteractionsPreset.apply(ref, params, plugin);
+      
+    } else if (structure.models.some(m => QualityAssessment.isApplicable(m, 'pLDDT'))) {
+      return await QualityAssessmentPLDDTPreset.apply(ref, params, plugin);
+    } else if (structure.models.some(m => QualityAssessment.isApplicable(m, 'qmean'))) {
+      return await QualityAssessmentQmeanPreset.apply(ref, params, plugin);
+    } else {
+      return await PresetStructureRepresentations.auto.apply(ref, params, plugin);
+    }
+  }
+});
+
+
 const Molstar = props => {
 
   const {
@@ -106,9 +183,16 @@ const Molstar = props => {
         [PluginConfig.Viewport.ShowSettings, showSettings],
         [PluginConfig.Viewport.ShowSelectionMode, showSelectionMode],
         [PluginConfig.Viewport.ShowAnimation, showAnimation],
+        [PluginConfig.Structure.DefaultRepresentationPreset, ViewerAutoPreset.id],
         [PluginConfig.Viewport.ShowTrajectoryControls, showTrajectoryControls],
       ];
-      plugin.current = await createPluginUI(parentRef.current, spec);
+      plugin.current = await createPluginUI(parentRef.current, spec, {
+        onBeforeUIRender: plugin => {
+            // the preset needs to be added before the UI renders otherwise
+            // "Download Structure" wont be able to pick it up
+            plugin.builders.structure.representation.registerPreset(ViewerAutoPreset);
+        }
+    });
       if (!showAxes) {
         // eslint-disable-next-line
         plugin.current.canvas3d?.setProps({
@@ -158,10 +242,10 @@ const Molstar = props => {
     }
   }, [showAxes])
 
+  
 
   const loadStructure = async (modelFile, trajFile, plugin) => {
     if (plugin) {
-      //console.log(plugin.dataFormats);
       plugin.clear();
       if (trajFile) {
         await addTrajectory(plugin, {
@@ -182,6 +266,12 @@ const Molstar = props => {
         });
       } else if (modelFile.data) {
         const asset = Asset.File(new File([modelFile.data], modelFile.name));
+        // const data = await OpenFiles(plugin, {
+        //   files: [asset],
+        //    format: { name: 'auto', params: {} },
+        //    visuals: true
+        // });
+        //PluginCommands.State.Snapshots.OpenFile(plugin, { file: new File([modelFile.data], modelFile.name)});
         plugin.runTask(plugin.state.data.applyAction(OpenFiles, {
           files: [asset],
            format: { name: 'auto', params: {} },
