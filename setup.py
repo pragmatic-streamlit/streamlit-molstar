0ec1deced6dc8c3252eaf78a63048d53a780acfc
luowf <louwf@dp.tech>
2023-08-27 00:32:50 +0800

    fix pacakge

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='streamlit-molstar',
-    version='0.4.1',
+    version='0.4.2',
     author='mapix',
     author_email='mapix.me@gmail.com',
     packages=find_packages(),
